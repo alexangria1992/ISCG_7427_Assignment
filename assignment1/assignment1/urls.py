ef97210b3d8b36aa8f27c2c756a0510f44f6784c
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-05-09 21:39:15 +1200

    Created Calendar front end and back end

--- assignment1/assignment1/urls.py
+++ assignment1/assignment1/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register-user'),
     path('myprofile/', views.my_profile, name='my-profile'),
+    path('calendar/', views.calendar, name = 'calendar')
 
 
 ]
