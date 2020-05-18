51ff0f7cd7d989ce1b5213a889204b892f285c59
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-05-19 00:56:15 +1200

    Installed Weurzerg and Added a Profile template for teach in new profile-section.html

--- assignment1/settings.py
+++ assignment1/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     
     'django.contrib.admin',
     'django.contrib.auth',
+    'django_werkzeug',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
