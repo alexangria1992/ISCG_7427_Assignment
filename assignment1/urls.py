b7aa3067cc56c910f43f1cdd1f1ac4aa53add185
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-05-27 05:36:25 +1200

    Forgot Password Templates established

--- assignment1/urls.py
+++ assignment1/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from amsApp import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,7 +18,22 @@ urlpatterns = [
     path('update/child/', views.updateChild, name = 'update-child'),
     path('register/', views.register_view, name='register-user'),
     path('remove/child/', views.remove_child, name = 'remove-child'),
-    path('supervise/', views.supervise, name = 'supervise'),
+    path('supervise/', views.supervise, name='supervise'),
+   path('forgot/',
+     auth_views.PasswordResetView.as_view(template_name="forgot.html"),
+     name="forgot"),
+    path('reset_password_sent/',
+            auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+            name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+        name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+            auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+            name="password_reset_complete"),
+
 
 
 
