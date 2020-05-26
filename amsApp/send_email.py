c2ec8648345eb78e43992547ba2995969540b628
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-05-27 04:43:38 +1200

    Tried New API Key

--- amsApp/send_email.py
+++ amsApp/send_email.py
@@ -5,7 +5,7 @@ from sendgrid.helpers.mail import Mail
 def sendEmailWithSendGrid(customMessage):
 
     message = Mail(
-    from_email='alex040892@gmail.com',
+    from_email='alexangria1992@gmail.com',
     to_emails=customMessage["to_emails"],
     subject=customMessage["subject"],
     plain_text_content=customMessage["plain_text_content"],
@@ -14,7 +14,7 @@ def sendEmailWithSendGrid(customMessage):
 
 
     try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient ('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')
         response = sg.send(message)
         print('THE RESPONSE CODE IS:' + str(response.status_code))
         print(response.body)
